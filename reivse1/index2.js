8825704a97d09b03908c8cb30ff3e5f163dfc1e7
sriramede9 <only4programming03@gmail.com>
2019-12-20 12:25:08 -0500

    baby steps that got into promises

--- reivse1/index2.js
+++ reivse1/index2.js
@@ -2,37 +2,62 @@ console.log("the start");
 
 //shows callback Hell or christmas tree problem
 
-getUser(1, user => {
-  console.log(user);
+// getUser(1, user => {
+//   console.log(user);
 
-  getReposByUserName(user.gitUserName, repos => {
-    console.log(repos);
+//   getReposByUserName(user.gitUserName, repos => {
+//     console.log(repos);
 
-    getcommitsbyUser(repos[0], commits => {
-      console.log(commits);
-    });
-  });
-});
+//     getcommitsbyUser(repos[0], commits => {
+//       console.log(commits);
+//     });
+//   });
+// });
+
+// getUser(1)
+//   .then(user => {
+//     getReposByUserName(user.gitUserName);
+//   })
+//   .then(repos => {
+//     console.log(repos);
+//   });
+
+// getUser(1).then(user => console.log(user));
+
+// getReposByUserName("sri").then(repo => console.log(repo));
+
+// getCommitsbyRepo("repo1").then(commits => console.log(commits));
+
+getUser(1)
+  .then(user => getReposByUserName(user.gitUserName))
+  .then(repos => getCommitsbyRepo(repos[0]))
+  .then(commits => console.log(commits));
 
 console.log("the end");
 
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log("getting user from data base with id \t" + id);
-    callback({ id: id, gitUserName: "sriramede9" });
-  }, 2000);
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("getting user from data base with id \t" + id);
+      resolve({ id: id, gitUserName: "sriramede9" });
+    }, 2000);
+  });
 }
 
-function getReposByUserName(username, callback) {
-  setTimeout(() => {
-    console.log("here are the repos for the username \t" + username);
-    callback(["repo1", "repo2", "repo3"]);
-  }, 2000);
+function getReposByUserName(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("here are the repos for the username \t" + username);
+      resolve(["repo1", "repo2", "repo3"]);
+    }, 2000);
+  });
 }
 
-function getCommitsbyRepo(repo, callback) {
-  setTimeout(() => {
-    console.log("here are the commits for the repo \t" + repo);
-    callback(["com1", "com2", "com3"]);
-  }, 2000);
+function getCommitsbyRepo(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("here are the commits for the repo \t" + repo);
+      resolve(["com1", "com2", "com3"]);
+    }, 2000);
+  });
 }
